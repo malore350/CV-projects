95b97928d53de679e6f8c4ea77ccbdc1ed6b9ba4
Gasimov Kamran <kamran.qasimoff@kaist.ac.kr>
2023-10-24 22:28:11 +0900

    Final version of task 1

--- task1.py
+++ task1.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
-# Load the .mat file using mat73
-data_mat73 = sio.loadmat('face.mat')
-X = data_mat73['X']
+# TASK A
+# Load the .mat file
+data_mat = sio.loadmat('face.mat')
+X = data_mat['X']
 
 num_faces = 10  # number of images per identity
 num_identities = X.shape[1] // num_faces
+print(f'Number of identities: {num_identities}')
 
 # Partition the data into training and testing sets using mat73-loaded data
 train_data = np.hstack([X[:, i*num_faces:i*num_faces+8] for i in range(num_identities)])
@@ -23,14 +25,6 @@ mean_face_pca = np.mean(train_images, axis=0)
 mean_centered_data = train_images - mean_face_pca
 mean_centered_image = np.mean(mean_centered_data, axis=0)
 
-#plot mean centered face
-plt.figure(figsize=(5, 5))
-plt.imshow(mean_centered_image, cmap='gray')
-plt.title('Mean Centered Face')
-plt.axis('off')
-plt.show()
-
-
 # Flatten the mean-centered data for PCA
 A = mean_centered_data.reshape(mean_centered_data.shape[0], -1).T
 
@@ -41,16 +35,18 @@ print(rank_S)
 
 # Calculate eigenvectors and eigenvalues
 eigenvalues, eigenvectors = np.linalg.eigh(S)
-sorted_indices = np.argsort(eigenvalues)[::-1]
+sorted_indices = np.argsort(eigenvalues)[::-1]      # sort eigenvalues in descending order
 eigenvalues = eigenvalues[sorted_indices]
 eigenvectors = eigenvectors[:, sorted_indices]
 
+eigenvectors = eigenvectors / np.linalg.norm(eigenvectors, axis=0)
+
 # Visualizing the mean face and the first few eigenfaces
-# plt.figure(figsize=(5, 5))
-# plt.imshow(mean_face_pca, cmap='gray')
-# plt.title('Mean Face')
-# plt.axis('off')
-# plt.show()
+plt.figure(figsize=(5, 5))
+plt.imshow(mean_face_pca, cmap='gray')
+plt.title('Mean Face')
+plt.axis('off')
+plt.show()
 
 
 fig, axes = plt.subplots(1, 5, figsize=(15, 5))
@@ -73,4 +69,67 @@ plt.grid(True)
 plt.show()
 
 # We can pick rank_S - 1 eigenfaces to represent the data. After that, the eigenvalues are ~ 0. So, we can pick 414 eigenfaces to represent the data.
+# We have rank(S) - 1 eigenvectors with nonzero eigenvalues.
 
+
+# TASK B
+# Compute the alternate covariance matrix
+S_2 = (A.T @ A) / num_identities
+
+# Calculate eigenvectors and eigenvalues for the alternate covariance matrix
+eigenvalues_2, eigenvectors_2 = np.linalg.eigh(S_2)
+sorted_indices_2 = np.argsort(eigenvalues_2)[::-1]  # sort eigenvalues in descending order
+eigenvalues_2 = eigenvalues_2[sorted_indices_2]
+eigenvectors_2 = eigenvectors_2[:, sorted_indices_2]
+
+# Transform the eigenvectors back to the original space
+eigenvectors_transformed = A @ eigenvectors_2
+# Normalize the transformed eigenvectors
+eigenvectors_transformed = eigenvectors_transformed / np.linalg.norm(eigenvectors_transformed, axis=0)
+
+# Visualizing the first few eigenfaces for the alternate method
+fig, axes = plt.subplots(1, 5, figsize=(15, 5))
+for i in range(5):
+    eigenface = eigenvectors_transformed[:, i].reshape(56, 46)
+    axes[i].imshow(eigenface, cmap='gray')
+    axes[i].set_title(f'Eigenface {i+1} (Alternate)')
+    axes[i].axis('off')
+plt.tight_layout()
+plt.show()
+
+# Plot the sorted eigenvalues for the alternate method
+plt.figure(figsize=(10, 6))
+plt.plot(eigenvalues_2)
+plt.xlabel('Eigenvector Index (Alternate)')
+plt.ylabel('Eigenvalue Magnitude (Alternate)')
+plt.title('Eigenvalues in Descending Order (Alternate)')
+plt.grid(True)
+plt.show()
+
+# Comparing the eigenvalues from both methods
+plt.figure(figsize=(10, 6))
+plt.plot(eigenvalues, label='From S')
+plt.plot(eigenvalues_2, label='From S_alternate')
+plt.xlabel('Eigenvector Index')
+plt.ylabel('Eigenvalue Magnitude')
+plt.title('Comparison of Eigenvalues')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+# Initialize an empty list to hold the cosine similarities
+cosine_similarities = []
+
+# Calculate cosine similarity for the first five pairs of eigenvectors
+for i in range(5):
+    cos_sim = np.dot(eigenvectors[:, i], eigenvectors_transformed[:, i])
+    cosine_similarities.append(cos_sim)
+
+# Print the cosine similarities
+print("Cosine Similarities for the first five pairs of eigenvectors:", cosine_similarities)
+
+# Cosine Similarities for the first five pairs of eigenvectors: [-1.0000000000000004, -1.0000000000000009, 0.9999999999999988, 0.9999999999999991, 1.0]
+# The cosine similarities are not exactly 1 because of numerical errors.
+# Negative cosine similarity means they are exactly on the same vector, just pointing in the opposite direction.
+# For this reason we can see that the images are very similar, just the pixels are inverted (where it is dark it becomes light and vice versa)
