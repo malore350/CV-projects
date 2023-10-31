040bc9ad6f8cb9eb387971e9bbd1f854d5210f97
Gasimov Kamran <kamran.qasimoff@kaist.ac.kr>
2023-10-31 14:06:00 +0900

    Added comments

--- task1_2.py
+++ task1_2.py
@@ -8,6 +8,15 @@ import time
 
 import seaborn as sns
 
+import psutil
+
+# Measure memory usage
+def get_memory_usage():
+    process = psutil.Process()
+    mem_info = process.memory_info()
+    return mem_info.rss / (1024 ** 2)  # rss: Resident Set Size, in bytes
+
+
 # TASK A
 # Load the .mat file
 data_mat = sio.loadmat('face.mat')
@@ -67,32 +76,32 @@ def reconstruct_image(image, eigenvectors, mean_face, num_bases):
     return reconstructed_image, error
 
 # Sample usage
-num_bases_list = [5, 100, 200, 300, 414, 1000]  # Number of bases to use for reconstruction
+num_bases_list = [5, 100, 200, 300, 415, 1000]  # Number of bases to use for reconstruction
 sample_images = [train_images[0], test_data[:, 0].reshape(46, 56).T, train_images[-1]]  # Sample images from training and testing datasets
 # sample_images = [test_data[:, 0].reshape(46, 56).T, test_data[:, 2].reshape(46, 56).T, test_data[:, 4].reshape(46, 56).T]
 
 # Loop through each sample image and reconstruct it using different numbers of bases
-for i, image in enumerate(sample_images):
-    plt.figure(figsize=(15, 5))
-    plt.subplot(1, len(num_bases_list) + 1, 1)
-    plt.imshow(image, cmap='gray')
-    plt.title('Original')
-    plt.axis('off')
+# for i, image in enumerate(sample_images):
+#     plt.figure(figsize=(15, 5))
+#     plt.subplot(1, len(num_bases_list) + 1, 1)
+#     plt.imshow(image, cmap='gray')
+#     plt.title('Original')
+#     plt.axis('off')
     
-    for j, num_bases in enumerate(num_bases_list):
-        reconstructed_image, error = reconstruct_image(image, eigenvectors, mean_face_pca, num_bases)
-        plt.subplot(1, len(num_bases_list) + 1, j + 2)
-        plt.imshow(reconstructed_image, cmap='gray')
-        plt.title(f'Reconstructed\n{num_bases} bases\nError: {error:.2f}')
-        plt.axis('off')
+#     for j, num_bases in enumerate(num_bases_list):
+#         reconstructed_image, error = reconstruct_image(image, eigenvectors, mean_face_pca, num_bases)
+#         plt.subplot(1, len(num_bases_list) + 1, j + 2)
+#         plt.imshow(reconstructed_image, cmap='gray')
+#         plt.title(f'Reconstructed\n{num_bases} bases\nError: {error:.2f}')
+#         plt.axis('off')
     
-    plt.tight_layout()
-    plt.show()
+#     plt.tight_layout()
+#     plt.show()
 
 
 # TASK B
 # TRAINING with NN
-num_bases = 416  # Number of bases to use for reconstruction
+num_bases = 415  # Number of bases to use for reconstruction
 # Project training and test data onto the PCA bases
 train_features = (train_data - mean_face_pca.flatten()[:, np.newaxis]).T @ eigenvectors[:, :num_bases]
 test_features = (test_data - mean_face_pca.flatten()[:, np.newaxis]).T @ eigenvectors[:, :num_bases]
@@ -104,12 +113,18 @@ test_labels = np.array([[i] * 2 for i in range(num_identities)]).flatten()
 # Step 2: Classification
 # Initialize and train k-NN classifier
 start_time = time.time()
-knn = KNeighborsClassifier(n_neighbors=1)
+knn = KNeighborsClassifier(n_neighbors=1, algorithm='ball_tree', p=1)
 knn.fit(train_features, train_labels)
+# Using algorithms ball_tree and kd_tree for kNN is faster than brute force/auto
+# Using p = 1 (Manhattan distance) is more accurate than p = 2 (Euclidean distance)
+# Check for mem usage with leaves
 
 # Predict labels for test data
 predicted_labels = knn.predict(test_features)
 end_time = time.time()
+
+memory_usage = get_memory_usage()   # check the memory usage
+print(f'Final memory usage: {memory_usage:.2f} MB')
 
 # Step 3: Evaluation
 # Calculate recognition accuracy
@@ -127,4 +142,31 @@ print(f'Time taken: {time_taken:.2f} seconds')
 sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues")
 plt.ylabel('True label')
 plt.xlabel('Predicted label')
+plt.show()
+
+
+# Select specific indices from your test set (replace these with the indices of the faces you're interested in)
+specific_indices = [0, 10, 20]  # For example, to test on the first, 11th, and 21st faces in your test set
+
+# Select corresponding features and labels
+specific_test_features = test_features[specific_indices]
+specific_test_labels = test_labels[specific_indices]
+
+# Make predictions using k-NN for these specific faces
+specific_predictions = knn.predict(specific_test_features)
+
+# Output the results
+for i, (true_label, predicted_label) in enumerate(zip(specific_test_labels, specific_predictions)):
+    match = "Yes" if true_label == predicted_label else "No"
+    print(f"Test face {i+1}: True label = {true_label}, Predicted label = {predicted_label}, Match = {match}")
+
+# If you want to show these specific test faces
+for i, index in enumerate(specific_indices):
+    specific_face_image = test_data[:, index].reshape(46, 56).T
+    plt.subplot(1, len(specific_indices), i+1)
+    plt.imshow(specific_face_image, cmap='gray')
+    plt.title(f"Label: {specific_test_labels[i]}\nPrediction: {specific_predictions[i]}")
+    plt.axis('off')
+
+plt.tight_layout()
 plt.show()
